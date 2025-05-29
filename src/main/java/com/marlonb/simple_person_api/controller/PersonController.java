cb5bd0e22f16adb1ed8e6934355e90d294a94c3e
Acrexia07 <barnuevo.marlon07@gmail.com>
2025-05-29 17:04:51 +0800

    "Relocate @RequestMapping annotation"

--- src/main/java/com/marlonb/simple_person_api/controller/PersonController.java
+++ src/main/java/com/marlonb/simple_person_api/controller/PersonController.java
@@ -1,8 +1,7 @@
 package com.marlonb.simple_person_api.controller;
 
 import com.marlonb.simple_person_api.model.Person;
-import org.springframework.stereotype.Component;
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
